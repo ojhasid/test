30c8aa2ac23d5999b7a5ce9b94c383eac703566b
Sid Ojha <60242480+ojhasid@users.noreply.github.com>
2020-06-12 23:59:53 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,12 +1,2 @@
 def start():
-    
-    print("Starting program with many second delay")
-    led = Pin(2,Pin.OUT)
-
-    while(1):
-        led.value(1)
-        time.sleep(2)
-        led.value(0)
-        time.sleep(2)
-        
-start()
+    print("Testing transfer v3.3.4")
