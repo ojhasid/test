6813cf6fa7a579569031e1abc8c4d865750d7f3d
Sid Ojha <60242480+ojhasid@users.noreply.github.com>
2020-06-13 00:04:55 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,2 +1,12 @@
-def testIt():
-    print("Testing transfer v3.3.4")
+import machine
+
+def start():
+    print("Led blinking with 2s delay")
+    
+    while(1):
+        led = machine.Pin(2, Pin.OUT)
+        led.high()
+        sleep(1)
+        led.low()
+        sleep(1)
+    
