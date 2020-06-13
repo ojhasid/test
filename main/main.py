d66e4126bd6f20205551c3a48de6c794fcc5af77
Sid Ojha <60242480+ojhasid@users.noreply.github.com>
2020-06-13 00:08:56 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,12 +1,12 @@
 import machine
 
 def start():
-    print("Led blinking with 2s delay")
+    print("Led blinking with 2sasdasd delay")
     
-    while(1):
-        led = machine.Pin(2, Pin.OUT)
-        led.high()
-        sleep(1)
-        led.low()
-        sleep(1)
+    #while(1):
+        #led = machine.Pin(2, Pin.OUT)
+        #led.high()
+        #sleep(1)
+        #led.low()
+        #sleep(1)
     
